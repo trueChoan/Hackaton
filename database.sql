cc896c6752f686177507905f8b9883c3eea767ea
unknown <ala.johan@gmail.com>
2022-05-12 17:22:54 +0200

    logo

--- database.sql
+++ database.sql
@@ -74,6 +74,7 @@ CREATE TABLE `user` (
   `email` VARCHAR(100) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY(`id`)
+) ENGINE = InnoDB;
 
 CREATE TABLE `donation` (
 	`id` INT NOT NULL AUTO_INCREMENT,
